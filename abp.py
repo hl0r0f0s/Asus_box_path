e35069ff03ef9b497aec3ee2ad1ea87a76d9bf28
hlorofos <hlorofos90@gmail.com>
2024-06-12 14:57:01 +0800

    minor impproves

--- abp.py
+++ abp.py
@@ -2,7 +2,21 @@ from pathlib import Path
 from win32com.client import Dispatch
 import getpass
 import os
+import click
 
+print('')
+print('░  ░░░░  ░░  ░░░░░░░░░      ░░░       ░░░░      ░░░        ░░░      ░░░░      ░░')
+print('▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒')
+print('▓        ▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓  ▓▓       ▓▓▓  ▓▓▓▓  ▓▓      ▓▓▓▓  ▓▓▓▓  ▓▓▓      ▓▓')
+print('█  ████  ██  ████████  ████  ██  ███  ███  ████  ██  ████████  ████  ████████  █')
+print('█  ████  ██        ███      ███  ████  ███      ███  █████████      ████      ██')                                                                               
+print('')
+print('░░      ░░░      ░░  ░░░░  ░░      ░░░░░░░       ░░░      ░░  ░░░░  ░░░░░░       ░░░      ░░        ░  ░░░░  ░')
+print('▒  ▒▒▒▒  ▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒  ▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒▒  ▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒▒▒▒  ▒▒▒▒  ▒▒▒▒  ▒')
+print('▓  ▓▓▓▓  ▓▓      ▓▓  ▓▓▓▓  ▓▓      ▓▓▓▓▓▓▓       ▓▓  ▓▓▓▓  ▓▓▓    ▓▓▓▓▓▓▓▓       ▓▓  ▓▓▓▓  ▓▓▓▓  ▓▓▓▓        ▓')
+print('█        ███████  █  ████  ███████  ██████  ████  █  ████  ██  ██  ███████  ███████        ████  ████  ████  █')
+print('█  ████  ██      ███      ███      ███████       ███      ██  ████  ██████  ███████  ████  ████  ████  ████  █')                                                                                                                    
+print('')
 
 def create_shortcut(file_name: str, target: str, work_dir: str, arguments: str = ''):
     shell = Dispatch('WScript.Shell')
@@ -17,12 +31,20 @@ path = Path(abs_file_name)
 
 user = getpass.getuser()
 
+link = f"C:\\Users\\{user}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Asus_box_path.lnk"
+
 create_shortcut(
-    file_name=f"C:\\Users\\{user}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Asus_box_path.lnk",
+    file_name= link,
     target=str(path),
     work_dir=str(path.parent),
     arguments='/start StrixControl',
 )
 
-os.startfile (f"C:\\Users\\{user}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Asus_box_path.lnk")
+print(f'[info] Shortcut create! Path: \n {link}')
+
+os.startfile (link)
+
+print(f'[info] Shortcut started! Finish!')
+
+click.pause('[input] Press Enter to exit...')
 
